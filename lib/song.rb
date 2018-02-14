8520453ff137396b2ca5c29e7bace61347bf46f0
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-14 21:02:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,20 @@
 class Song
   attr_accessor :name, :artist
-  
+
   def initialize(name)
     @name = name
   end
- 
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
+    self.artist.add_song(self)
   end
-    
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+
+  def self.new_by_filename(filename)
+    song = self.new(filename.split(" - ")[1])
+    song.artist_name=(filename.split(" - ")[0])
+    song
   end
+
 end
+
